d8da9117c0dec36f8d9637cad879ec32d8ee8799
Approximations <datkidbosst@gmail.com>
2023-03-31 22:01:05 -0300

    adicionado formulário de controle

--- src/pages/Resume/index.tsx
+++ src/pages/Resume/index.tsx
@@ -4,6 +4,8 @@ import {FaDollarSign} from 'react-icons/fa';
 import './index.css';
 
 function Resume() {
+
+
     return (
         <div className='resume-container'>
             <div className="card-wrapper">
@@ -11,6 +13,30 @@ function Resume() {
                 <Card title='Saídas' value='R$1000' icon={<FaDollarSign size={20}></FaDollarSign>}></Card>
                 <Card title='Total' value='R$1000' icon={<FaDollarSign size={20}></FaDollarSign>}></Card>
             </div>
+
+            <form className="control-form">
+                <div className='input-wrapper'>
+                    <label htmlFor="description">Descrição</label>
+                    <input id='description' type="text" placeholder='Insira a descrição'/>
+                </div>
+                <div className='input-wrapper'>
+                    <label htmlFor="value">Valor</label>
+                    <input id='value' type="text" placeholder='Insira o valor'/>
+                </div>
+        
+                <div className="radio-wrapper">
+                    <label>
+                        <input id='in' type="radio" name='type'/>
+                        Entrada
+                    </label>
+                    <label>
+                        <input id='out' type="radio" name='type'/>
+                        Saída
+                    </label>
+                </div>
+
+                <button type="submit" className='submit-button'>ADICIONAR</button>
+            </form>
         </div>
     );
 }
